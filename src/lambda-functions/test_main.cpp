c15a4eba0a56534864f2da65e4deccb00f58f09d
Ajay.Gupt <Ajay.Gupt@LT0137551.in.ril.com>
2025-11-23 19:25:54 +0530

    Lambda Functions with examples added

--- src/lambda-functions/test_main.cpp
+++ src/lambda-functions/test_main.cpp
@@ -3,5 +3,15 @@ using namespace std;
 
 int main() {
     cout << "Hello, World!" << endl;
+    []() {
+        cout << "This is a lambda function declaration without call in C++17." << endl;
+    };
+    []() {
+        cout << "This is a lambda function declaration & call in C++17." << endl;
+    }();
+    auto fun = []() {
+        cout << "This is a lambda function in C++17." << endl;
+    };
+    fun(); // Calling the lambda function
     return 0;
 }
